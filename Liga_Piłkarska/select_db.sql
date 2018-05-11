6e0e985a2fadb4db556700391f34d95217b3198e
MikiCyg <miki@comsite.com.pl>
2018-05-11 19:51:39 +0200

    select

---
+++ Liga_Piłkarska/select_db.sql
@@ -0,0 +1,13 @@
+--Występowanie złączenia pomiędzy drużyną a zawodnikiem za pomocą INNER JOIN
+SELECT * FROM drużyna INNER JOIN zawodnik z on drużyna.id = z.id
+;
+--Grupowanie zawodników po ilości bramek
+SELECT nazwisko FROM zawodnik GROUP BY nazwisko HAVING count (nazwisko) > 1
+;
+--Arytmetyczne wyliczenie rzeczywistego czasu rozgrywki
+SELECT *, rzeczywista_godzina_zakoń-rzeczywista_godzina_rozp
+  AS rzeczywisty_czas_meczu
+    FROM mecz ORDER BY rzeczywisty_czas_meczu
+;
+--Warunki odwołujące się do wzorców napisów
+SELECT * FROM drużyna WHERE nazwa LIKE '%płock%'
